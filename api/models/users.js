f25e4f5c334206be336fda1cfffddfbcf821714e
Frances NG <fng@reactive.co.jp>
2017-02-19 14:23:17 +0900

    add email verification step on registration

--- api/models/users.js
+++ api/models/users.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
-
+import mailgun from 'mailgun-js';
+import moment from 'moment';
+import jwtDecode from 'jwt-decode';
 import nconf from '../config';
 import { getSession, Users } from '../utils/neo4j';
 import { newError, dbError } from '../utils/errorHandler';
@@ -21,21 +23,77 @@ import { PublicUser, User, SessionUser } from './neo4j/user';
 */
 
 const register = (req, res, next) => {
-  const { username } = req.body;
+  const { username, email } = req.body;
   const session = getSession(req);
-  session.run(Users.read({ username }), { username })
-    .then((results) => {
-      if (!_.isEmpty(results.records)) {
+  session.run(Users.ifExists({ username }, { email }), { username, email })
+  .then((results) => {
+    if (!_.isEmpty(results.records)) {
+      const existUsername = results.records[0].get('username');
+      const existEmail = results.records[0].get('email');
+      if (existUsername && existEmail) {
+        throw newError(400, 'username/email already in use');
+      } else if (existUsername) {
         throw newError(400, 'username already in use');
+      } else if (existEmail) {
+        throw newError(400, 'email already in use');
+      }
+      return passport.authenticate('local-signup', (err, user, status) => {
+        if (err || !user) throw newError(400, status.message);
+        const newUser = new User(user);
+        console.log(newUser)
+        const verifyToken = jwt.sign(newUser, nconf.get('verify_secret'), { expiresIn: '1d' });
+        const mg = mailgun({apiKey: nconf.get('MAILGUN_API_KEY'), domain: nconf.get('MAILGUN_DOMAIN')});
+
+        const data = {
+          from: 'Sonders <account@verify.sonders.co>',
+          to: newUser.email,
+          subject: 'Please confirm your account',
+          html: `<html><h1>SONDERS</h1><h2>Email address confirmation</h2><p>Thank you for signing up. Please verify your email address by clicking the following link:<a href="http://localhost:3000/api/v0/verify?click=${verifyToken}">CLICK HERE</a></html>`
+        };
+
+        mg.messages().send(data, function (error, body) {
+          console.log(body);
+        });
+        return res.status(200).json({
+          success: true,
+          user: newUser,
+          message: status.message,
+        });
+      })(req, res, next);
+    } else {
+      throw newError(400, 'DB error');
+    }
+  })
+  .catch((err) => {
+    next(dbError(err));
+  })
+};
+
+const verify = (req, res, next) => {
+  const session = getSession(req);
+  const jwt = jwtDecode(req.query.click);
+  if (jwt.exp && !moment(jwt.exp * 1000).isAfter()) {
+    next(dbError(newError(403, 'Verification token has expired')));
+  }
+  const { id } = jwt;
+
+  const params = _.extend({
+    verified: true,
+  }, { id });
+
+  session.run(Users.update(params), params)
+    .then((updated) => {
+      if (!updated.records[0]) {
+        throw newError(404, 'Unable to verify');
+      }
+      const updatedUser = updated.records[0].get('user');
+      if (updatedUser) {
+        res.status(200).json({
+          message: 'successfully verified account',
+          user: new User(updatedUser),
+        });
       } else {
-        return passport.authenticate('local-signup', (err, user, status) => {
-          if (err || !user) throw newError(400, status.message);
-          return res.status(200).json({
-            success: true,
-            user: new User(user),
-            message: status.message,
-          });
-        })(req, res, next);
+        throw newError(400, 'Error with DB connection');
       }
     })
     .catch(err => (
@@ -49,7 +107,7 @@ const login = (req, res, next) => {
       return next(newError(400, err || status.message));
     }
     const newUser = new SessionUser(user);
-    const token = jwt.sign(newUser, nconf.get('jwt_secret'));
+    const token = jwt.sign(newUser, nconf.get('jwt_secret'), { expiresIn: '7d' });
     return res.status(200).json({
       success: true,
       token,
@@ -111,4 +169,5 @@ module.exports = {
   register,
   login,
   update,
+  verify,
 };
